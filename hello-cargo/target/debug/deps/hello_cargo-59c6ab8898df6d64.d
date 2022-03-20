8facc92783fd9355d58461623cf9647e512a8af9
ishitarastogi <ishita3099@gmail.com>
2022-03-20 17:14:49 +0530

    First Program

---
+++ hello-cargo/target/debug/deps/hello_cargo-59c6ab8898df6d64.d
@@ -0,0 +1,5 @@
+/Users/ishitarastogi/Documents/Rust-practice/hello-cargo/target/debug/deps/hello_cargo-59c6ab8898df6d64: src/main.rs
+
+/Users/ishitarastogi/Documents/Rust-practice/hello-cargo/target/debug/deps/hello_cargo-59c6ab8898df6d64.d: src/main.rs
+
+src/main.rs:
---
+++ hello-cargo/target/debug/deps/hello_cargo-fdc0779e98252fbf.d
@@ -0,0 +1,5 @@
+/Users/ishitarastogi/Documents/Rust-practice/hello-cargo/target/debug/deps/hello_cargo-fdc0779e98252fbf.rmeta: src/main.rs
+
+/Users/ishitarastogi/Documents/Rust-practice/hello-cargo/target/debug/deps/hello_cargo-fdc0779e98252fbf.d: src/main.rs
+
+src/main.rs:
